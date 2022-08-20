bfdb33f8c37c0067f331f8df3a4145f96fcd35e6
Rodrigo Argenton <rodrigolk4321@gmail.com>
2022-08-19 23:30:45 -0400

    Desafio 53 - python

---
+++ desafio53.py
@@ -0,0 +1,15 @@
+#Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços. Exemplos de palíndromos:APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO, ANOTARAM A DATA DA MARATONA.
+
+frase = str(input("Digite uma frase: ")).strip() .upper()
+
+palavras = frase.split()
+junto = "".join(palavras)
+inverso = ""
+
+for letra in range(len(junto) -1, -1 , -1):
+    inverso += junto[letra]
+
+if inverso == junto:
+    print("Temos um palíndromo!")
+else:
+    print("A frase digitada não é um palidromo.")
