46588b206c2d6ba64a299773eeb32538b8864e22
Rodrigo Argenton 2 <rodrigolk4321@gmail.com>
2023-01-19 13:33:59 -0400

    teste

---
+++ desafio73.py
@@ -0,0 +1,9 @@
+#Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre:
+
+times = ('Corinthias', 'Palmeiras', 'Santos', 'Grêmio', 'Cruzeiro', 
+        'Flamengo', 'Vasco', 'Chapecoense', 'Atlético', 'Bota fogo', 
+        'Atlético-PR', 'Bahia', 'São Paulo', 'Fluminense', 'Sport Recife', 
+        'EC Victória', 'Coritiba', 'Avaí', 'Ponte Preta', 'Atlético-Go'
+)
+
+print
