c35e34d781bbd3723c34b88a2466b27c5d75a6b9
Rodrigo Argenton 2 <rodrigolk4321@gmail.com>
2023-01-19 13:42:14 -0400

    Desafio 73 - python

--- desafio73.py
+++ desafio73.py
@@ -6,4 +6,7 @@ times = ('Corinthias', 'Palmeiras', 'Santos', 'Grêmio', 'Cruzeiro',
         'EC Victória', 'Coritiba', 'Avaí', 'Ponte Preta', 'Atlético-Go'
 )
 
-print
+print(f"Os 5 primeiros colocados são: {times[:5]}")
+print(f"Os 4 ultimos colocados são: {times[-4:]}")
+print(f"Segue times em ordem alfabetica: {sorted(times)}")
+print(f"O time da chapecoense está na posição: {times.index('Chapecoense')+1}")
