fe1ddb2fc75c328b0c462a20a28e27a3e057e5b4
Rodrigo Argenton <rodrigolk4321@gmail.com>
2023-03-13 14:15:55 -0400

    comment tv in 'desafio120'

--- 4 - complementos/1 - Class/desafio120/tv.py
+++ 4 - complementos/1 - Class/desafio120/tv.py
@@ -1,5 +1,15 @@
 class tv:
+    '''
+    Classe com o objetivo de controlocar o volume, mostrar o volume atual e mudar o canal
+    função:
+    volume;
+    print_volume;
+    mudar_canal;
+    '''
     def __init__(self, ligar=False, desligar=False, canal=0, volumeatual=0):
+        '''
+        função inicial para começar a classe.
+        '''
         self.ligar = ligar
         self.desligar = desligar
         self.canal = canal
@@ -7,6 +17,13 @@ class tv:
         print(f'status atual da tv: {self.ligar}')
 
     def volume(self, aumentar=0, diminuir=0):
+        '''
+        aumentar ou diminuor o volume
+
+        parametros:
+        aumentar;
+        diminuir;
+        '''
         while self.volumeatual + aumentar > 100:
             aumentar = int(input(f'O valor inserido {aumentar} ultrapassa o volume 100, uma vez que o volume atual é {self.volumeatual}, logo, por favor insira um valor valido: '))
         self.volumeatual += aumentar
@@ -15,9 +32,18 @@ class tv:
         self.volumeatual -= diminuir
 
     def print_volume(self):
+        '''
+        mostra o volume atual
+        '''
         print(f'O volume atual é: {self.volumeatual}')
 
     def mudar_canal(self, valor):
+        '''
+        muda o cananal dentro de uma faixa de 0 até 1000
+
+        parametro:
+        valor;
+        '''
         while 0 >= valor <= 1000:
             valor = int(input('Canal não existe! digite outro canal: '))
         self.canal = valor
