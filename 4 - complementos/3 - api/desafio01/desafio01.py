7a116386f43d016ac4c05d35c720e6270c5c2222
Rodrigo Argenton <rodrigolk4321@gmail.com>
2023-04-08 09:15:22 -0400

    add mongodb and def 'novoUser'

--- 4 - complementos/3 - api/desafio01/desafio01.py
+++ 4 - complementos/3 - api/desafio01/desafio01.py
@@ -1,11 +1,8 @@
 from validacao import login
 
 while True:
-    email = str(input("Digite o seu E-mail: "))
-    senha = str(input("Digite a sua senha: "))
+    # email = str(input("Digite o seu E-mail: "))
+    # senha = str(input("Digite a sua senha: "))
     
-    acesso = login(email, senha)
+    acesso = login(email='rodrigo@gmail.com', senha='senha')
     acesso.validacao()
-    
-    if acesso.returnoValidacao() == True:
-        break
--- 4 - complementos/3 - api/desafio01/validacao.py
+++ 4 - complementos/3 - api/desafio01/validacao.py
@@ -1,20 +1,35 @@
 class login:
     def __init__(self, email, senha):
+        from dotenv import load_dotenv
+        from pymongo import MongoClient
+        import os
+        load_dotenv()   # carrega o arquivo .env
+        MONGO_URI = os.getenv("MONGO_URI")  # recebe o valor que está na variavel "MONGO_URI" que está no arquivo .env
+        cliente = MongoClient(MONGO_URI)    # encurta o processo de conexão com os bancos de dados existentes no mongo
+        db = cliente.novouser
+        colecao = db['users']
+        self.colecao = colecao
         self.email = email
         self.senha = senha
         self.sucesso = False
 
     def validacao(self):
-        email = "teste@gmail.com"
-        senha = "senha123"
-        if email == self.email and senha == self.senha:
-            self.sucesso = True
-            print("sucesso.")
-        else:
-            print("E-mail ou senha incorreto.")
+        for colecao in self.colecao.find():
+            if colecao["email"] == self.email and colecao["senha"] == self.senha:
+                self.sucesso = True
+                print("sucesso.")
+            else:
+                print("E-mail ou senha incorreto.")
     
-    def returnoValidacao(self):
+    def returnValidacao(self):
         if self.sucesso == True:
             return True
         else:
             print("Error")
+
+    def novoUser(self):
+        user = {}
+        user['email'] = self.email
+        user['senha'] = self.senha
+        print(user)
+        addcolecao = self.colecao.insert_one(user)
